b64f010e57ab68aa8e00ab27de4492b95655ff11
Tonio1308 <46360672+Tonio1308@users.noreply.github.com>
2026-08-21 22:44:57 -0600

    UPD Prod App Update Link

--- Functions/Start/StartFunctions.cs
+++ Functions/Start/StartFunctions.cs
@@ -35,8 +35,9 @@ namespace HeroServer
                                                     "https://drive.google.com/drive/u/0/folders/1DWYN4kU6SwK9fO1YmihQrqDA_frWsBbl|<None>");
                     if (WebEnvConfig.Env == EnvironmentType.PROD)
                         return new StartResponse(0, "0|Heroes Migrantes|Tu App está desactualizada.\r\nPor favor actualízala e intenta de nuevo.",
-                                                    "https://drive.google.com/drive/u/0/folders/1gzP8z6hc70tq0aZqM9q4UNDnygokw6OK|<None>");
-                                                    //"https://play.google.com/store/apps/details?id=com.Hero.Migrant|https://apps.apple.com/gt/app/hero-migrant/idxxxxxxxxxx");
+                                                    "https://play.google.com/store/apps/details?id=com.hero.migrant|<None>");
+                                                    //"https://play.google.com/apps/internaltest/4700485194528386850|<None>");
+                                                    //"https://play.google.com/store/apps/details?id=com.hero.migrant|https://apps.apple.com/gt/app/hero-migrant/idxxxxxxxxxx");
                     return new StartResponse(0, "0|Heroes Migrantes|¡Tienes que actualizar tu App!");
                 }
             }
